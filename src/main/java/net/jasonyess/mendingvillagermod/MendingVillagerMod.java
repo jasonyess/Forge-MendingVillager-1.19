127c1887ab500d8156c7ba5a91bff62f747d7797
jasonyess <jasonyes.gd@gmail.com>
2024-11-17 09:46:45 -0500

    Added 2 items

--- src/main/java/net/jasonyess/mendingvillagermod/MendingVillagerMod.java
+++ src/main/java/net/jasonyess/mendingvillagermod/MendingVillagerMod.java
@@ -1,6 +1,7 @@
 package net.jasonyess.mendingvillagermod;
 
 import com.mojang.logging.LogUtils;
+import net.jasonyess.mendingvillagermod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,6 +22,8 @@ public class MendingVillagerMod
     public MendingVillagerMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
